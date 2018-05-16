7d0645b8ac35a0050d519a618d7d4a14a0b77be9
Lukas Scheuerle <lukas.scheuerle@web.de>
2018-05-16 14:21:55 +0200

    Laying Groundwork for GPS Implementation

---
+++ Groundbreaker/Assets/Scripts/GPSHandler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GPSHandler : MonoBehaviour {
+
+	private GameController GC;
+
+	void Start()
+	{
+		try 
+		{
+			GC = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+		} catch (Exception)
+		{
+			throw;
+		}
+
+		if (Config.UseDebugGpsPosition)
+		{
+			GC.CurrentGpsPosition = Config.DebugGpsPosition;
+		}
+		else
+		{
+			if(Input.location.isEnabledByUser)
+			{
+				Input.location.Start();
+			}
+		}
+
+	}
+
+}
---
+++ Groundbreaker/Assets/Scripts/Helper.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Helper {
+
+	public class LocationData
+	{
+		public double Timestamp { get; set; }
+        public float Latitude { get; set; }
+        public float Longitude { get; set; }
+        public float Altitude { get; set; }
+        public float HorizontalAccuracy { get; set; }
+        public float VerticalAccuracy { get; set; }
+		public string Status { get; set; }	
+	}
+
+	public static Vector2 WorldToTilePos(float lat, float lon, int zoom)
+    {
+        Vector2 p = new Vector2
+        {
+            x = (lon + 180.0f) / 360.0f * (1 << zoom),
+            y = (1.0f - Mathf.Log(Mathf.Tan(lat * Mathf.PI / 180.0f) + 1.0f / Mathf.Cos(lat * Mathf.PI / 180.0f)) / Mathf.PI) / 2.0f * (1 << zoom)
+        };
+
+        return p;
+    }
+
+}
