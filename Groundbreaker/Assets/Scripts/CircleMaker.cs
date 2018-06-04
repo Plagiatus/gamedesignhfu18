22cef677f8e573bb25a2560cf2ca7940db13cd8e
Lukas Scheuerle <lukas.scheuerle@web.de>
2018-06-04 22:51:17 +0200

    don't ask. git's an asshole.

---
+++ Groundbreaker/Assets/Scripts/CircleMaker.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class CircleMaker : MonoBehaviour {
+
+	[Range(0,100)]
+    public int segments = 50;
+	float radius;
+    LineRenderer line;
+	bool drawn = false;
+	GameController GC;
+	void Start () {
+		line = gameObject.GetComponent<LineRenderer>();
+
+		line.positionCount = segments + 1;
+		line.useWorldSpace = false;
+		line.loop = true;
+
+		GC = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!drawn && GC.circle != null)
+		{
+			DrawCircle();
+			drawn = true;
+		}
+	}
+
+	public void DrawCircle(){
+		// Debug.Log((GC.circle.radius / Config.TileSizeInRL) * Config.TileSizeInGame);
+		float radius = (GC.circle.radius / Config.TileSizeInRL) * Config.TileSizeInGame;
+		float x, y, z;
+		float angle = 20f;
+		y = 0.1f;
+
+		for(int i = 0; i < (segments + 1); i++){
+			x=Mathf.Sin(Mathf.Deg2Rad * angle) * radius;
+			z=Mathf.Cos(Mathf.Deg2Rad * angle) * radius;
+			line.SetPosition (i, new Vector3(x, y, z));
+
+			angle += (360f / segments);
+		}
+	}
+}
---
+++ Groundbreaker/Assets/Scripts/MapObjects.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapObjects : MonoBehaviour {
+
+	GameObject circleInGame;
+	GameController GC;
+	private int previousTileX;
+	private int previousTileY;
+	void Start () {
+		try 
+		{
+			GC = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+			circleInGame = GameObject.Find("Circle");
+		} catch
+		{
+			throw;
+		}
+	}
+	
+	void Update () {
+		if(GC.circle != null && (previousTileX != GC.CurrentGpsPosition.OsmTilePosition.x || previousTileY != GC.CurrentGpsPosition.OsmTilePosition.y))
+		{
+			previousTileX = Mathf.FloorToInt(GC.CurrentGpsPosition.OsmTilePosition.x);
+			previousTileY = Mathf.FloorToInt(GC.CurrentGpsPosition.OsmTilePosition.y);
+			Vector3 circlePosition = Helper.LocationToGamePosition(GC.circle.center, GC);
+			circleInGame.transform.localPosition = circlePosition;
+		}	
+	}
+}
