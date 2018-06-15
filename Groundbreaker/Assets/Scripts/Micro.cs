2d786d838cb3afefb2c75949f0ec4a886f2b9dd4
Marlene <marlene.dillig@gmx.net>
2018-06-15 12:47:21 +0200

    Minigame Micro Scene added

---
+++ Groundbreaker/Assets/Scripts/Micro.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(AudioSource))]
+public class Micro : MonoBehaviour
+{
+    public float sensitivity = 60;
+    public float loudness = 0;
+    public GameObject _cube;
+    public new AudioSource audio;
+
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+        audio.clip = Microphone.Start(null, true, 10, 44100);
+        audio.loop = true;
+        while (!(Microphone.GetPosition(null) > 0)) { }
+        audio.Play();
+    }
+
+    void Update()
+    {
+        loudness = GetAveragedVolume() * sensitivity;
+        if (loudness > 6)
+        {
+            _cube.SetActive(true);
+        }
+    }
+
+    float GetAveragedVolume()
+    {
+        float[] data = new float[256];
+        float a = 0;
+        audio.GetOutputData(data, 0);
+        foreach (float s in data)
+        {
+            a += Mathf.Abs(s);
+        }
+        return a / 256;
+    }
+}
