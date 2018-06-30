63afb64e595e6b64a0cf4dbbf6ca610af4532ac1
Marlene <marlene.dillig@gmx.net>
2018-06-30 09:53:27 +0200

    New Script for Minigame_Micro

---
+++ Groundbreaker/Assets/Scripts/StormLoader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StormLoader : MonoBehaviour {
+
+    public GameObject stormPrefab;
+
+	// Use this for initialization
+	void Start () {
+
+        Vector3 stormPos = new Vector3(0.0f, 1.0f, 0.0f);
+
+        GameObject storm = Instantiate(stormPrefab, stormPos, Quaternion.identity);
+		
+	}
+	
+
+}
